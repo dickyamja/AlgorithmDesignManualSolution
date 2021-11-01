40b66214dd754a5d930fff62478fc350dcba5707
ahmad.muzakkir <ahmad.muzakkir@advotics.com>
2021-11-02 04:12:44 +0700

    add material SingleLinkedList delete existing node

--- src/main/java/material/linkedlist/SingleLinkedList.java
+++ src/main/java/material/linkedlist/SingleLinkedList.java
@@ -18,7 +18,6 @@ public class SingleLinkedList {
     }
 
 
-
     public boolean insertNewNodeAtSpecificLocation(int value, int locationIndex){
         boolean result = false;
         if(head == null){
@@ -50,6 +49,73 @@ public class SingleLinkedList {
         return result;
     }
 
+    private boolean deleteNodeAtSpecificLocation(int locationIndex){
+        if(head == null){
+            return false;
+        }
+
+        if(locationIndex == 0){
+            head = head.getNextNode();
+            return true;
+        }
+
+        int counter = 1;
+        Node tmpNode = head.getNextNode();
+        if(tmpNode == null){
+            head.setNextNode(null);
+            tail.setNextNode(null);
+            return true;
+        }
+
+        do {
+            if(counter == locationIndex - 1){
+                if(tmpNode.getNextNode() != null){
+                    Node tempNextNode = tmpNode.getNextNode();
+                    if(tempNextNode.getNextNode() != null){
+                        tmpNode.setNextNode(tempNextNode.getNextNode());
+                        return true;
+                    }else{
+                        tmpNode.setNextNode(null);
+                        return true;
+                    }
+                }
+            }else{
+                tmpNode = tmpNode.getNextNode();
+            }
+            counter++;
+        }while (counter <= locationIndex);
+
+        return false;
+    }
+
+    public String searchNode(int value){
+        String result = "";
+        if (head == null){
+            result = "value " + value + " is not exist in the linkedlist";
+        }else{
+            int counter = 0;
+            Node tempNode = head.getNextNode();
+            while (tempNode.getNextNode() != null){
+                counter++;
+                if(tempNode.getValue() == value){
+                    result = "value " + value + " is found at node " + counter;
+                    break;
+                }else{
+                    tempNode = tempNode.getNextNode();
+                }
+
+            }
+
+            // check if the value is exist at last node
+            if(tempNode.getValue() == value){
+                result = "value " + value + " is found at node " + counter;
+            }else{
+                result = "value " + value + " is not found at linkedlist ";
+            }
+        }
+        return result;
+    }
+
     public void printALlNode(){
         if(head == null){
             System.out.println("the linkedList is still empty");
@@ -57,7 +123,7 @@ public class SingleLinkedList {
             System.out.println("Head  value : " + head.getValue());
             System.out.println(" ==================================== ");
             Node node = head;
-            int counter = 1;
+            int counter = 0;
             while (node.getNextNode() != null){
                 System.out.println("Node " + counter + " value : " + node.getValue());
                 node = node.getNextNode();
@@ -78,9 +144,9 @@ public class SingleLinkedList {
         tail = node;
     }
 
-    private void insertLastNode(Node currentNode, int value){
+    private void insertLastNode(Node currentLastNode, int value){
         Node newNode = new Node(value, null);
-        currentNode.setNextNode(newNode);
+        currentLastNode.setNextNode(newNode);
         tail = newNode;
     }
 
@@ -92,6 +158,18 @@ public class SingleLinkedList {
         singleLinkedList.insertNewNode(2);
         singleLinkedList.insertNewNodeAtSpecificLocation(3, 3);
         singleLinkedList.insertNewNodeAtSpecificLocation(10, 6);
+        singleLinkedList.printALlNode();
+        System.out.println(singleLinkedList.searchNode(14));
+        System.out.println(singleLinkedList.searchNode(2));
+        System.out.println(singleLinkedList.searchNode(45));
+
+        System.out.println(singleLinkedList.deleteNodeAtSpecificLocation(0));
+        singleLinkedList.printALlNode();
+
+        System.out.println(singleLinkedList.deleteNodeAtSpecificLocation(2));
+        singleLinkedList.printALlNode();
+
+        System.out.println(singleLinkedList.deleteNodeAtSpecificLocation(1));
         singleLinkedList.printALlNode();
     }
 }
